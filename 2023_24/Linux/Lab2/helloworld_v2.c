eabb87195957d6f1a066fc542ccb262429723811
K4rl1t0s <carlo.nicolo@hotmail.com>
2024-11-15 21:39:28 +0100

    Fix

--- 2023_24/Linux/Lab2/helloworld_v2.c
+++
@@ -1,4 +0,0 @@
-#include<stdio.h>
- 
-int main(){
-printf("Hello to my hello world C program version version 2\n ");}
--- 2023_24/Linux/Lab2/helloworld_v2.sh
+++ 2023_24/Linux/Lab2/helloworld_v2.sh
@@ -5,7 +5,9 @@ touch helloworld_v2.c
 echo "#include<stdio.h>" > helloworld_v2.c
 echo " " >> helloworld_v2.c
 echo "int main(){"  >> helloworld_v2.c
-echo "printf(\"Hello to my hello world C program version version 2\n \");}" >> helloworld_v2.c
+echo "printf(\"Hello to my hello world C program version version 2 \");" >> helloworld_v2.c
+echo "return 0;" >> helloworld_v2.c
+echo "}" >> helloworld_v2.c
 
 echo " "
 echo "cat helloworld_v2.c"
@@ -22,6 +24,7 @@ echo "Permessi chmod +x"
 chmod +x hello_v2
 echo "Esecuzione programma ./hello_v2"
 ./hello_v2
+echo " "
 echo "Rimozione cartella"
 cd .. && rm -R helloworld_v2
 echo "Cartella rimossa"
